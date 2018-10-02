0ba89c9664de363f616ed940c7818ac57e21e84c
Jacob <jacobsan.dev@gmail.com>
2018-10-02 23:04:56 +0530

    initial

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const logger = require('morgan');
+const bodyParser = require('body-parser');
+
+// Set up express app
+const app = express();
+
+// Log requests to console
+app.use(logger('dev'));
+
+// Parse incoming request data
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
+// Setup a default catch-all route that sends back a response msg in JSoN
+app.get('*', (req, res) => res.status(200).send({
+    message: 'Welcome to the beginning of nothingness'
+}))
+
+module.exports = app;
